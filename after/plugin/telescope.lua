24aecb1707b6ec6a9523632c9d81da9ba0503bb2
Jimmy Poulsen <jimmy@poulsen.dk>
2024-04-17 16:01:30 +0200

    Use dropdown as theme for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,13 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+local actions = require('telescope.actions')
+local themes = require('telescope.themes')
 
+-- Define keymaps
+vim.keymap.set('n', '<leader>pf', function() builtin.find_files(themes.get_dropdown()) end, {})
+vim.keymap.set('n', '<C-p>', function() builtin.git_files(themes.get_dropdown()) end, {})
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string(themes.get_dropdown({
+        search = vim.fn.input("Grep > ")
+    }))
+end, {})
+vim.keymap.set('n', '<leader>vh', function() builtin.help_tags(themes.get_dropdown()) end, {})
